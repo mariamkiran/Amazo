0f166b2efd1a9a8afb3f4a1c6a0e08d5e6bb08dd
mariamkiran <mariam.kiran@gmail.com>
2015-06-06 13:11:53 +0100

    check

---
+++ Amazo/src/test.py
@@ -0,0 +1,17 @@
+'''
+Created on 6 Jun 2015
+
+@author: mkiran
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
